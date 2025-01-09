ebd93aa5c1abbf7d18e79cef1e3afebd1de142c0
mtandao centre <mtandaocentre@gmail.com>
2025-01-09 22:26:19 +0300

    Render Navbar component in the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <div 
       /*Create padding to allow for screen size responsiveness*/
       className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'
     >
-      Hello World
+      {/*Render Navbar component in the main app */}
+      <Navbar />
     </div>
   )
 }
