fe5c368ed794efb28e8bb39f057162d78823a96f
mtandao centre <mtandaocentre@gmail.com>
2025-01-09 19:02:30 +0300

    Create padding to allow for screen size responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div 
+      /*Create padding to allow for screen size responsiveness*/
+      className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'
+    >
+      Hello World
+    </div>
   )
 }
 
