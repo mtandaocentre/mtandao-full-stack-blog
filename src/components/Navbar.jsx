630cc29ee29175dd3a3fddc209860df21d10b1b7
mtandao centre <mtandaocentre@gmail.com>
2025-01-09 22:40:58 +0300

    Add responsivenes for mobile screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 const Navbar = () => {
   return (
     <div className=''>
-        {/*Add logo */}
+        
+        {/* Add logo */}
         <div className="">
             <img src="/mtandao-logo.png" className="w-10 h-10" alt="Mtandao Logo" />
         </div>
+
+        {/*Add responsivenes for mobile screen*/}
+        <div className="md:hidden">Mobile Screen</div>
+
+        {/**/}
     </div>
   )
 }
