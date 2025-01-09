f1019acc571ef651a1550834cf1fafcc38547a96
mtandao centre <mtandaocentre@gmail.com>
2025-01-09 22:43:33 +0300

    Add responsivenes for desktop screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,9 @@ const Navbar = () => {
         {/*Add responsivenes for mobile screen*/}
         <div className="md:hidden">Mobile Screen</div>
 
+          {/*Add responsivenes for desktop screens*/}
+          <div className="hidden md:flex">Mobile Screen</div>
+
         {/**/}
     </div>
   )
