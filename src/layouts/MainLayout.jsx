e08bbeb272a6c1dc50e7973526cde2fee4da19fc
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 18:34:49 +0300

    Create main layout to handle Navbar in all pages

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+
+// Create main layout to handle Navbar in all pages
+const MainLayout = () => {
+  return (
+    <div 
+        className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'
+    >
+        <Navbar />
+        <Outlet />
+    </div>
+  )
+}
+
+export default MainLayout
