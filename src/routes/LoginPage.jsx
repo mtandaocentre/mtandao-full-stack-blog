6ee069bf12458cd09874f5dead7f0d21f40294c3
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 19:56:56 +0300

    Add sign up url path to lead to register page

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,10 +1,11 @@
 import { SignIn } from "@clerk/clerk-react"
 
 // Add clerk signIn component and style it
+// Add sign up url path to lead to register page
 const LoginPage = () => {
   return (
     <div className='flex items-center justify-center h-[calc(100vh-80px)]'>
-      <SignIn />
+      <SignIn signUpUrl="/register"/>
     </div>
   )
 }
