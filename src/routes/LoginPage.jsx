8da6749197555b037e819b3e1333e655862759fd
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 19:54:30 +0300

    Add clerk signIn component and style it

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,6 +1,11 @@
+import { SignIn } from "@clerk/clerk-react"
+
+// Add clerk signIn component and style it
 const LoginPage = () => {
   return (
-    <div className=''>LoginPage</div>
+    <div className='flex items-center justify-center h-[calc(100vh-80px)]'>
+      <SignIn />
+    </div>
   )
 }
 
