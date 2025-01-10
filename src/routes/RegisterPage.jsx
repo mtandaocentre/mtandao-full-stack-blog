8fb98f8195f4089bf229e874463144d2055d6127
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 20:03:40 +0300

    Added clerk sign up component and signInUrl path

--- src/routes/RegisterPage.jsx
+++ src/routes/RegisterPage.jsx
@@ -1,6 +1,11 @@
+import { SignUp } from "@clerk/clerk-react"
+
+// Added clerk sign up component and signInUrl path
 const RegisterPage = () => {
   return (
-    <div className=''>RegisterPage</div>
+    <div className='flex items-center justify-center h-[calc(100vh-80px)]'>
+      <SignUp signInUrl="/login"/>
+    </div>
   )
 }
 
