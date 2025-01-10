b42bc7eb9e7d85b9865e1596723667223c2b33d0
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 16:11:11 +0300

    create router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { 
+  createBrowserRouter,
+  routerProvider,
+  Route,
+  Link 
+} from 'react-router-dom'
+
+// create router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path:"about",
+    element: <div>About</div>v
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
