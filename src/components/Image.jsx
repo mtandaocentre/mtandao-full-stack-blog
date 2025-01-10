c7d3cbb9f3247d82e81d9c794e6405c92ec68d3f
mtandao centre <mtandaocentre@gmail.com>
2025-01-10 14:38:31 +0300

    Corrected prop declaration

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,12 +1,12 @@
 import { IKImage } from "imagekitio-react"
 
 // Make image component a prop
-const Image = (src, className, w, h, alt) => {
+// Corrected prop declaration
+const Image = ({src, className, w, h, alt}) => {
   return (
     <IKImage 
         urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT} 
         path={src} 
-        src="https://ik.imagekit.io/mtandaocentre/mtandao-logo.png"
         className={className} 
         loading="lazy"
         lqip={{ active: true, quality: 20 }}
