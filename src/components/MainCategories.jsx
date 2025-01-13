cb066a873199f3635c720ce8cc88234ea6e45e4a
mtandao centre <mtandaocentre@gmail.com>
2025-01-13 10:29:26 +0300

    Add and style serch bar

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -57,7 +57,26 @@ const MainCategories = () => {
         <span className="text-xl font-medium">|</span>
 
         {/* Search */}
-        <div className="">Search</div>
+        {/* Add and style serch bar */}
+        <div 
+            className="bg-[#e0e0e0] p-2 rounded-full flex items-center 
+            gap-2"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="20"
+                height="20"
+                fill="none"
+                stroke="#0a0a0a"
+            >
+                <circle cx="10.5" cy="10.5" r="7.5" />
+                <line x1="16.5" y1="16.5" x2="22" y2="22"/>
+            </svg>
+            <input type="text" placeholder="Search" 
+                className="font-medium bg-transparent" 
+            />
+        </div>
     </div>
   )
 }
