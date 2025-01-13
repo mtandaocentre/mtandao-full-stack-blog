3b136676e0f34adaec650ed041536b0a4f81891a
mtandao centre <mtandaocentre@gmail.com>
2025-01-13 09:41:30 +0300

    Add text color and font

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -1,12 +1,17 @@
 const MainCategories = () => {
   return (
     <div 
-        /* Create main catogories back ground */
+        /*  - Create main catogories back ground
+            - Add text color and font
+        */
         className='hidden md:flex bg-[#a3a3a3] rounded-3xl 
         xl:rounded-full p-4 shadow-lg items-center justify-center
-        gap-8'
+        gap-8 text-[#1b1c1c] font-bold'
     >
-        MainCategories
+        {/* Links */}
+        <div className="">Links</div>
+        {/* Search */}
+        <div className="">Search</div>
     </div>
   )
 }
