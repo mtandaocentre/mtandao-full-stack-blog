df909ce33cf2a6c00d784972c62723d8a289112e
mtandao centre <mtandaocentre@gmail.com>
2025-01-13 09:31:30 +0300

    Create main catogories back ground

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -1,6 +1,13 @@
 const MainCategories = () => {
   return (
-    <div className=''>MainCategories</div>
+    <div 
+        /* Create main catogories back ground */
+        className='hidden md:flex bg-[#a3a3a3] rounded-3xl 
+        xl:rounded-full p-4 shadow-lg items-center justify-center
+        gap-8'
+    >
+        MainCategories
+    </div>
   )
 }
 
