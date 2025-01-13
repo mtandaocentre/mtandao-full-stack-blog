70b9e114694899d485b49fab92a2b9b7b4f7f3f9
mtandao centre <mtandaocentre@gmail.com>
2025-01-13 09:52:05 +0300

    Created all posts category

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const MainCategories = () => {
   return (
     <div 
@@ -9,7 +11,18 @@ const MainCategories = () => {
         gap-8 text-[#1b1c1c] font-bold'
     >
         {/* Links */}
-        <div className="">Links</div>
+        <div 
+            className="flex-1 flex items-center justify-between 
+            flex-wrap"
+        >
+            {/* Created all post category */}
+            <Link to="/posts" 
+                className="bg-[#1b1c1c] text-[#e0e0e0] rounded-full
+                px-4 py-2"
+            >
+                All Posts
+            </Link>
+        </div>
         {/* Search */}
         <div className="">Search</div>
     </div>
