b4dc40a8b4b2002410275aef68512c051e8166e2
mtandao centre <mtandaocentre@gmail.com>
2025-01-13 11:54:09 +0300

    Add and style image component

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,3 +1,5 @@
+import Image from "./Image"
+
 const FeaturedPosts = () => {
   return (
     /* Add responsiveness */
@@ -5,7 +7,15 @@ const FeaturedPosts = () => {
         {/* Divide featured post into two sections */}
         
         {/* Fisrt post */}
-        <div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
+        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        
+        {/* Image */}
+        {/* Add and style image component */}
+        <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+
+        {/* Details */}
+        {/* Title */}
+        </div>
 
         {/* Other posts */}
         <div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
