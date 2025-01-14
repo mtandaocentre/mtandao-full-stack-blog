aa5eec4ee862b2d7eb1770c2bfe3b55c025beaa6
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 12:56:53 +0300

    Style title and detail container

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -5,10 +5,14 @@ const SinglePostPage = () => {
     <div className='flex flex-col gap-8'>
       {/* Heading */}
       <div>
-        <div className=""></div>
+        {/* Title and Details */}
+        {/* Style title and detail container */}
+        <div className="lg:w-3/5 flex flex-col gap-8">
+
+        </div>
 
         {/* Image */}
-        {/* Add amd style Image component*/}
+        {/* Add and style Image component*/}
         <div className="hidden lg:block w-2/5">
           <Image src="postImg.jpeg" />
         </div>
