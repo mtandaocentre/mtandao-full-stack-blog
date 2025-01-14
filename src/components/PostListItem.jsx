53f4b296ba9304509b913fd99e05df388c7a5626
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 10:08:03 +0300

    Add and style PostListItem image

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -1,8 +1,14 @@
+import Image from "./Image"
+
 const PostListItem = () => {
   return (
     /* Style PostListItem container */
     <div className='flex flex-col xl:flex-row gap-8'>
-
+        {/* Image */}
+        {/* Add and style PostListItem image */}
+        <div className="md:hidden xl:block">
+            <Image src="postImg.jpeg" className="rounded-2xl object-cover"/>
+        </div>
     </div>
   )
 }
