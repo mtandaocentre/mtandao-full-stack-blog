681174e925c9b60daa110acac4ec97b273ae11fc
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 16:40:43 +0300

    Add social media links

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -118,6 +118,26 @@ const SinglePostPage = () => {
               h="48"
             />
           </div>
+
+          {/* Add user name and description */}
+          <Link>Mr. Mtandao</Link>
+          <p className="">
+            Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry. Lorem Ipsum has been the industrys 
+            standard dummy text ever since the 1500s, when an unknown 
+            printer took a galley of type and scrambled it to make a 
+            type specimen book. 
+          </p>
+
+          {/* Add social media links */}
+          <div className="flex gap-2">
+            <Link>
+              <Image src="facebook.svg"/>
+            </Link>
+            <Link>
+              <Image src="instagram.svg"/>
+            </Link>
+          </div>
         </div>
 
       </div>
