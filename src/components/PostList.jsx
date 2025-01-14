615d60a35aa7bc7cb4e4e230b8e6f4748655f7b4
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 10:45:13 +0300

    Add more post list items to post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,9 +2,15 @@ import PostListItem from "./PostListItem"
 
 const PostList = () => {
   return (
-    /* Style post list container */
+    /* - Style post list container 
+       - Add more post list items to post list
+    */
     <div className='flex flex-col gap-12 mb-8'>
         {/* Add PostListItem component to PostList */}
+        <PostListItem />
+        <PostListItem />
+        <PostListItem />
+        <PostListItem />
         <PostListItem />
     </div>
   )
