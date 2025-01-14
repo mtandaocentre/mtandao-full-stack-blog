55fcf129cc6e87b80b27dde81ddca0d853eb69f5
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 12:53:02 +0300

    Add amd style Image component

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,7 +1,21 @@
+import Image from "../components/Image"
 const SinglePostPage = () => {
   return (
     /* Style single post page container */
     <div className='flex flex-col gap-8'>
+      {/* Heading */}
+      <div>
+        <div className=""></div>
+
+        {/* Image */}
+        {/* Add amd style Image component*/}
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg" />
+        </div>
+
+      </div>
+      {/* Content */}
+
 
     </div>
   )
