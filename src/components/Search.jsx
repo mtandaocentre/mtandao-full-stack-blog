a16be462a138b305d5fa22a4a21f28931bbd4c0d
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 19:16:03 +0300

    Changed svg stroke

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,13 +4,14 @@ const Search = () => {
         className='bg-[#e0e0e0] p-2 rounded-full flex items-center 
         gap-2'
     >
+        {/* Changed svg stroke */}
         <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         width="20"
         height="20"
         fill="none"
-        stroke="gray"
+        stroke="#0f0f0f"
         >
             <circle cx="10.5" cy="10.5" r="7.5" />
             <line x1="16.5" y1="16.5" x2="22" y2="22" />
