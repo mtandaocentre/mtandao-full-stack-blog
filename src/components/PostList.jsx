032814e1dd54106fb539bec5613954cbda5e08b1
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 09:49:12 +0300

    Add PostListItem component to PostList

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,11 @@
+import PostListItem from "./PostListItem"
+
 const PostList = () => {
   return (
     /* Style post list container */
     <div className='flex flex-col gap-12 mb-8'>
-
+        {/* Add PostListItem component to PostList */}
+        <PostListItem />
     </div>
   )
 }
