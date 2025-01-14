bb91f0bb1ba4e40ff852ded767ce2559f0a60fca
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 13:10:43 +0300

    Add details container

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,17 +1,30 @@
 import Image from "../components/Image"
+import { Link } from "react-router-dom"
+
 const SinglePostPage = () => {
   return (
     /* Style single post page container */
     <div className='flex flex-col gap-8'>
       {/* Heading */}
-      <div>
+      <div className="flex gap 8">
+        
         {/* Title and Details */}
         {/* Style title and detail container */}
         <div className="lg:w-3/5 flex flex-col gap-8">
+          
           {/* Add title */}
           <h1>
             Your online place for everything computer
           </h1>
+
+          {/* Add details container */}
+          <div className="flex items-center gap-2 text-[#e0e0e0] text-sm">
+            <span>Written by</span>
+            <Link>Mr. Mtandao</Link>
+            <span>on</span>
+            <Link>Hardware</Link>
+            <span>2 days ago</span>
+          </div>
 
         </div>
 
