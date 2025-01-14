e7b5208a2d2ef088c41f9870647e8a0844b2240a
mtandao centre <mtandaocentre@gmail.com>
2025-01-14 14:58:19 +0300

    made description short

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -32,18 +32,14 @@ const SinglePostPage = () => {
             <span>2 days ago</span>
           </div>
 
-          {/* Add and style description */}
+          {/* - Add and style description 
+              - made description short */}
           <p className="text-[#e0e0e0] font-medium">
             Lorem Ipsum is simply dummy text of the printing and 
             typesetting industry. Lorem Ipsum has been the industrys 
             standard dummy text ever since the 1500s, when an unknown 
             printer took a galley of type and scrambled it to make a 
-            type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining 
-            essentially unchanged. It was popularised in the 1960s with 
-            the release of Letraset sheets containing Lorem Ipsum 
-            passages, and more recently with desktop publishing software 
-            like Aldus PageMaker including versions of Lorem Ipsum.
+            type specimen book. 
           </p>
 
         </div>
