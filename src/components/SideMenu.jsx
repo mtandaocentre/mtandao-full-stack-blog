e4f8aba77db46e1d2d39f8d0a0c528e51133900b
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 21:12:50 +0300

    Add categories container

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,4 +1,5 @@
 import Search from "./Search"
+import { Link } from "react-router-dom"
 
 const SideMenu = () => {
   return (
@@ -11,7 +12,23 @@ const SideMenu = () => {
         <Search />
 
         <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
+
+        {/* Categories */}
         <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+        {/* Add categories container  */}
+        <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline" to="/posts">All</Link>
+            <Link className="underline" to="/posts?cat=ai">AI</Link>
+            <Link className="underline" to="/posts?cat=cloud">Cloud</Link>
+            <Link className="underline" to="/posts?cat=data">Data</Link>
+            <Link className="underline" to="/posts?cat=hardware">Hardware</Link>
+            <Link className="underline" to="/posts?cat=iot">IoT</Link>
+            <Link className="underline" to="/posts?cat=security">Security</Link>
+            <Link className="underline" to="/posts?cat=software">Software</Link>
+            <Link className="underline" to="/posts?cat=web2">Web2</Link>
+            <Link className="underline" to="/posts?cat=web3">Web3</Link>
+
+        </div>
     </div>
   )
 }
