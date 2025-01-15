aafd637d97379713a33c32185c6d91f58afa7c7a
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 09:38:48 +0300

    Create comment input area

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,9 +2,14 @@ const Comments = () => {
   return (
     // Initiate and style Comments mother container
     <div className='flex flex-col gap-8 lg:w-3/5'>
-        
+
         {/* Add section title */}
         <h1 className="text-xl text-[#e0e0e0] underline">Comments</h1>
+
+        {/* Create comment input area */}
+        <div className="">
+            <textarea placeholder="Write a comment..." name="" id="" />
+        </div>
     </div>
   )
 }
