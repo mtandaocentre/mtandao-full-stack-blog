c1cc3202917ab0d38e7832bbbce71f103578e42a
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 11:21:10 +0300

    Created form with cover image button, article title and category chooser

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -2,7 +2,7 @@ import { useUser } from "@clerk/clerk-react"
 
 const Write = () => {
 
-// Check authentication
+    {/* Check authentication */}
     const {isloaded, isSignedin} = useUser()
 
     if(!isloaded){
@@ -20,6 +20,32 @@ const Write = () => {
         // Create page title
         <div className=''>
             <h1>Create a New Post</h1>
+
+            {/* Create form*/}
+            <form action="">
+                {/* Cover Image */}
+                <button>Add cover image</button>
+
+                {/* Article Title */}
+                <input type="text" placeholder="My Tech Opinion" />
+
+                {/* Category */}
+                <div className="">
+                    <label htmlFor="">Choose a category:</label>
+                    <select name="cat" id="">
+                        <option value="general">General</option>
+                        <option value="ai">AI</option>
+                        <option value="cloud">Cloud</option>
+                        <option value="data">Data</option>
+                        <option value="hardware">Hardware</option>
+                        <option value="iot">IoT</option>
+                        <option value="security">Security</option>
+                        <option value="software">Software</option>
+                        <option value="web2">Web2</option>
+                        <option value="web3">Web3</option>
+                    </select>
+                </div>
+            </form>
         </div>
     )
 }
