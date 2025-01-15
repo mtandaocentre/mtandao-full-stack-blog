8503659b1fd9af29821013aef574d8750795a571
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 16:16:20 +0300

    Check is user is authenticated

--- src/routes/WritePage.jsx
+++ src/routes/WritePage.jsx
@@ -1,4 +1,18 @@
+import { useUser } from "@clerk/clerk-react"
+
 const WritePage = () => {
+
+  // Check is user is authenticated
+  const {isLoaded, isSignedIn} =useUser()
+
+  if(!isLoaded){
+    return <div className="">Loading...</div>
+  }
+
+  if(isLoaded && !isSignedIn){
+    return <div className="">SignIn to Access this page.</div>
+  }
+
   return (
     <div className=''>WritePage</div>
   )
