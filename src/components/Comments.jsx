3d4489e6e412662cddb520a9a813b5b601106d57
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 09:34:48 +0300

    Add section title

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,7 +2,9 @@ const Comments = () => {
   return (
     // Initiate and style Comments mother container
     <div className='flex flex-col gap-8 lg:w-3/5'>
-
+        
+        {/* Add section title */}
+        <h1 className="text-xl text-[#e0e0e0] underline">Comments</h1>
     </div>
   )
 }
