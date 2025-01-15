3d93a231674d4fdfd17eaab2ce8cdd93a44330c2
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 20:58:38 +0300

    Add Search component

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,9 +1,15 @@
+import Search from "./Search"
+
 const SideMenu = () => {
   return (
     // Make menu sticky during scroll
     <div className='px-4 h-max sticky top-8'>
+        
         {/* Create side menu sections titles */}
         <h1 className="mb-4 text-sm font-medium">Search</h1>
+        {/* Add Search component */}
+        <Search />
+
         <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
         <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
     </div>
