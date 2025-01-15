e464a36c86098132ab3a545197ae7b08b0fb86a8
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 10:24:56 +0300

    Add comment place holder

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,6 +14,17 @@ const Comment = () => {
           <span className="font-medium">Mr. Mtandao</span>
           <span className="text-sm text-[#1b1c1c]">2 days ago</span>
       </div>
+
+      {/* Add comment place holder */}
+      <div className="mt-4">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and 
+          typesetting industry. Lorem Ipsum has been the industrys 
+          standard dummy text ever since the 1500s, when an unknown 
+          printer took a galley of type and scrambled it to make a 
+          type specimen book.
+        </p>
+      </div>
     </div>
   )
 }
