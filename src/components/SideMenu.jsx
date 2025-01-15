c392fcebefcfd870691adbc67676d3a255a0c260
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 20:50:18 +0300

    Create side menu sections titles

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,6 +1,11 @@
 const SideMenu = () => {
   return (
-    <div className=''>SideMenu</div>
+    <div className=''>
+        {/* Create side menu sections titles */}
+        <h1 className="mb-4 text-sm font-medium">Search</h1>
+        <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
+        <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+    </div>
   )
 }
 
