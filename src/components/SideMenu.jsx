808404f44d534f460992b2766c26ab861e047922
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 20:54:47 +0300

    Make menu sticky during scroll

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,6 +1,7 @@
 const SideMenu = () => {
   return (
-    <div className=''>
+    // Make menu sticky during scroll
+    <div className='px-4 h-max sticky top-8'>
         {/* Create side menu sections titles */}
         <h1 className="mb-4 text-sm font-medium">Search</h1>
         <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
