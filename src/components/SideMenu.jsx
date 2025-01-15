5b75ce0b0db8be01ab42b1d68464fd8084bf6a62
mtandao centre <mtandaocentre@gmail.com>
2025-01-15 21:50:49 +0300

    Add and style filter

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -11,7 +11,67 @@ const SideMenu = () => {
         {/* Add Search component */}
         <Search />
 
+        {/* Filter */}
         <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
+        {/* Add and style filter */}
+        <div className="flex flex-col gap-2 text-sm">
+            <label 
+                htmlFor="" 
+                className="flex items-center gap-2 cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="sort" 
+                        value="newest" 
+                        className="appearance-none w-4 h-4 border-[1.5px] 
+                        border-[#e0e0e0] cursor-pointer rounded-sm
+                        checked:bg-[#a3a3a3]"
+                    />
+                    Newest
+            </label>
+
+            <label 
+                htmlFor="" 
+                className="flex items-center gap-2 cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="sort" 
+                        value="popular" 
+                        className="appearance-none w-4 h-4 border-[1.5px] 
+                        border-[#e0e0e0] cursor-pointer rounded-sm
+                        checked:bg-[#a3a3a3]"
+                    />
+                    Most Popular
+            </label>
+
+            <label 
+                htmlFor="" 
+                className="flex items-center gap-2 cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="sort" 
+                        value="trending" 
+                        className="appearance-none w-4 h-4 border-[1.5px] 
+                        border-[#e0e0e0] cursor-pointer rounded-sm
+                        checked:bg-[#a3a3a3]"
+                    />
+                    Trending
+            </label>
+
+            <label 
+                htmlFor="" 
+                className="flex items-center gap-2 cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="sort" 
+                        value="oldest" 
+                        className="appearance-none w-4 h-4 border-[1.5px] 
+                        border-[#e0e0e0] cursor-pointer rounded-sm
+                        checked:bg-[#a3a3a3]"
+                    />
+                    Oldest
+            </label>
+
+        </div>
 
         {/* Categories */}
         <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
@@ -27,7 +87,6 @@ const SideMenu = () => {
             <Link className="underline" to="/posts?cat=software">Software</Link>
             <Link className="underline" to="/posts?cat=web2">Web2</Link>
             <Link className="underline" to="/posts?cat=web3">Web3</Link>
-
         </div>
     </div>
   )
