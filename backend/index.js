e750c4abf4abcdd5405c015ce3e85ce4dcb4d2e4
mtandao centre <mtandaocentre@gmail.com>
2025-01-16 13:35:14 +0300

    Tested Api request and .env access

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ import express from "express"
 // Create express application
 const app = express()
 
+// Create a test api request
+app.get("/test",(req,res)=>{
+    res.status(200).send("It works")
+})
+
+// test .env access 
+console.log(process.env.testkey)
+
 // Create port to listen
 app.listen(3000,()=>{
     console.log("Server is running")
