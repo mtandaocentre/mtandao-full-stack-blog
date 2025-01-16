e86ec05e28f74a8f7938c6dd96fe276fd8102f35
mtandao centre <mtandaocentre@gmail.com>
2025-01-16 14:00:12 +0300

    tested user router

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // Import Express
 import express from "express"
+import userRouter from "./routes/user.route.js"
 
 // Create express application
 const app = express()
@@ -7,12 +8,15 @@ const app = express()
 // Create a test api request
 app.get("/test",(req,res)=>{
     res.status(200).send("It works")
-})
+});
 
 // test .env access 
-console.log(process.env.testkey)
+console.log(process.env.testkey);
+
+// Test router
+app.use("/users", userRouter);
 
 // Create port to listen
 app.listen(3000,()=>{
     console.log("Server is running")
-})
+});
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,5 +3,10 @@ import express from "express"
 // Define router
 const router = express.Router()
 
+// Test router
+router.get("/anothertest",(req,res)=>{
+    res.status(200).send("user route")
+})
+
 // Export router
 export default router
