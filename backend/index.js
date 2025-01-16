14fe8fc89423db357147ca859e7951d84476407f
mtandao centre <mtandaocentre@gmail.com>
2025-01-16 11:07:31 +0300

    Create Express application and port to listen to

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,10 @@
-// Create Apllication
+// Import Express
 import express from "express"
+
+// Create express application
+const app = express()
+
+// Create port to listen
+app.listen(3000,()=>{
+    console.log("Server is running")
+})
