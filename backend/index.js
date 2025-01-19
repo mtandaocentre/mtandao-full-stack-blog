330418d45f332de7a61957b99c35de6de607c68e
mtandao centre <mtandaocentre@gmail.com>
2025-01-19 19:01:53 +0300

    Create and test env file

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import express from "express"
 // Create express app/server
 const app = express();
 
+// test if .env can be accessed
+console.log(process.env.test)
+
 // assign app port to listen to and report back
 app.listen(3000, () => {
     console.log("Server is running")
