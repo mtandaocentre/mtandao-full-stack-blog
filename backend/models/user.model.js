a44f1fd50f5468841de2ac52718cf0af86bdb6d9
mtandao centre <mtandaocentre@gmail.com>
2025-01-19 22:31:51 +0300

    Created models folder with userSchema

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose"
+import mongoose from "mongoose"
+
+// Create user Schema
+const userSchema = new Schema({
+        // username
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+
+        // email
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+
+        // Image
+        img:{
+            type:String,
+        },
+
+        // Saved Posts
+        svedPosts:{
+            type:[String],
+            default:[]
+        }
+    }, 
+
+    // Updates created @ time
+    { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
