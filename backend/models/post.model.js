852918db71f3650cc68f6f9f0bb82e028b6c1e6c
mtandao centre <mtandaocentre@gmail.com>
2025-01-20 00:36:14 +0300

    Create Post Schema

---
+++ backend/models/post.model.js
@@ -0,0 +1,48 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+// create post schema
+const postSchema = new Schema({
+        // Title
+        title: {
+            type: String,
+            required: true,
+        },
+
+        // Slug
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        // Description
+        desc: {
+            type: String,
+        },
+
+        // Content
+        content: {
+            type: String,
+            required: true,
+        },
+
+        // Check if post is featured
+        isFeatured: {
+            type: Boolean,
+            default: false,
+        },
+
+        // New post visit number
+        visit: {
+            type: Number,
+            default: 0
+        },
+
+    }, 
+
+    // Updates created time
+    {timestamps: true}
+);
+
+export default mongoose.model("Post", postSchema);
