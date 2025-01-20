7d01347a0baa8f5eec039ee227297c3cdb416bae
mtandao centre <mtandaocentre@gmail.com>
2025-01-20 16:27:02 +0300

    Use post conroller to get post for posts root endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,16 +1,12 @@
 import express from "express";
-import Post from "../models/post.model.js"
+import { getPosts } from "../controllers/post.controller.js";
 
 // Define router
 const router = express.Router();
 
 // Use router to call api
-// Test request with postman
-router.get("/", async (req, res) => {
-
-    const post = await Post.find();
-    res.status(200).send(post);
-});
+// Use post conroller to get post for posts root endpoint
+router.get("/", getPosts);
 
 // export default
 export default router
