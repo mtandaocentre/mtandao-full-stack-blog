7b5b22c6341314b2391cbce8c9c30bff65e68cc4
mtandao centre <mtandaocentre@gmail.com>
2025-01-20 16:36:33 +0300

    Declare and export getPost to fetch single post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,7 +1,13 @@
 import Post from "../models/post.model.js"
 
-// Declare and export getPost
+// Declare and export getPosts to fetch many posts
 export const getPosts = async (req, res) => {
-    const post = await Post.find();
+    const posts = await Post.find();
     res.status(200).send(posts);
+}
+
+// Declare and export getPost to fetch single post
+export const getPost = async (req, res) => {
+    const post = await Post.findOne({ slug: req.params.slug });
+    res.status(200).send(post);
 }
