160961c3ad57b4e51081e4e54cc1ae04812dd046
mtandao centre <mtandaocentre@gmail.com>
2025-01-20 15:52:31 +0300

    Test request with postman

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,12 +1,16 @@
 import express from "express";
+import Post from "../models/post.model.js"
 
 // Define router
 const router = express.Router();
 
 // Use router to call api
-router.get("/post-test", (req, res) => {
-    res.status(200).send("Post Route")
-})
+// Test request with postman
+router.get("/", async (req, res) => {
+
+    const post = await Post.find();
+    res.status(200).send(post);
+});
 
 // export default
 export default router
