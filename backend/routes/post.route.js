2cc3a157818aa1c244699efc4957fe043f59b6f2
mtandao centre <mtandaocentre@gmail.com>
2025-01-20 16:39:16 +0300

    Use post conroller to get single post

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getPosts } from "../controllers/post.controller.js";
+import { getPosts, getPost } from "../controllers/post.controller.js";
 
 // Define router
 const router = express.Router();
 
 // Use router to call api
-// Use post conroller to get post for posts root endpoint
+// Use post conroller to get posts for posts root endpoint
 router.get("/", getPosts);
+
+// Use post conroller to get single post
+router.get("/:slug", getPost);
 
 // export default
 export default router
