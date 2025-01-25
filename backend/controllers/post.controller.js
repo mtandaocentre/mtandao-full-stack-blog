0c0574831a14abfaca07d29d3bd49635e2965d1a
mtandao centre <mtandaocentre@gmail.com>
2025-01-25 22:01:40 +0300

    Declare and export craetePost to create a post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -11,3 +11,12 @@ export const getPost = async (req, res) => {
     const post = await Post.findOne({ slug: req.params.slug });
     res.status(200).send(post);
 }
+
+// Declare and export craetePost to create a post
+export const createPost = async (req, res) => {
+    const newPost = new Post(req.body);
+   
+    const post = await newPost.save();
+    res.status(200).send(post);
+
+}
