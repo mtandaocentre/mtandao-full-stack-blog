3d934f97729125a518e03e41a6ad34e2d1886786
mtandao centre <mtandaocentre@gmail.com>
2025-01-26 20:08:13 +0300

    Create clerk endpoint

--- backend/routes/webhook.route.js
+++ backend/routes/webhook.route.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { clerkWebHook } from "../controllers/webhook.controller";
 
 // define router
 const router = express.Router();
+
+// Create clerk endpoint
+router.post("/clerk", clerkWebHook);
 
 // Export router by default
 export default router
