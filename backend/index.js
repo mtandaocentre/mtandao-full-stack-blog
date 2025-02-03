a56a4c0abdc62fb925ee189ab0ce6c041fa8c015
mtandao centre <mtandaocentre@gmail.com>
2025-02-03 20:49:45 +0300

    Use clerkMiddleware in index.js

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,14 @@ import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 import webhookRouter from "./routes/webhook.route.js"
+import { clerkMiddleware } from "@clerk/express"
 
 
 // Create express app/server
 const app = express();
+
+// Use clerkMiddleWare
+app.use(clerkMiddleware);
 
 // Use webhook end point to call webhook router
 app.use("/webhooks", webhookRouter);
