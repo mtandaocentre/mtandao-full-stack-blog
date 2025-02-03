1c24a720f99dd360f1c9aaef9120d9b5579a26fb
mtandao centre <mtandaocentre@gmail.com>
2025-02-03 13:57:15 +0300

    Add user created data to database

--- backend/controllers/webhook.controller.js
+++ backend/controllers/webhook.controller.js
@@ -1,4 +1,50 @@
+import User from "../models/user.model.js"
+import Post from "../models/post.model.js"
+import Comment from "../models/comment.model.js"
+import { Webhook } from "svix";
+
 // Create clerkWebhook
 export const clerkWebHook = async (req, res) => {
+    
+    // Get webhook secret key
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-}
+    // If it doesnt exist throw an error
+    if (!WEBHOOK_SECRET) {
+        throw new Error("Webhook secret needed!")
+    }
+
+    // make verification
+    const payload = req.body;
+    const headers = req.headers;
+
+    const wh = new Webhook(WEBHOOK_SECRET);
+    let evt;
+    try {
+        evt = wh.verify(payload, headers);
+    } catch (err) {
+        res.status(400).json({
+            message: "Webhook verification failed!"
+        });
+    }
+
+    // console.log(evt.data);
+
+    // Add user created data to database
+    if (evt.type === 'user.created') {
+        
+        const newUser = new User({
+            clerkUserId: evt.data.id,
+            username: evt.data.username || evt.data.email_addresses[0].email_address,
+            email: evt.data.email_addresses[0].email_address,
+            img: evt.data.profile_img_url
+        });
+
+        await newUser.save()
+    };
+
+    return res.status(200).json({
+        message:"webhook received",
+    });
+
+};
