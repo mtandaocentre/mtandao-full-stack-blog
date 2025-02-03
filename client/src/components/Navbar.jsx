989ae45e1522a7951354cc14941d31a7e4f14db2
mtandao centre <mtandaocentre@gmail.com>
2025-02-03 21:39:54 +0300

    Get authentication token from react app using useAuth

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import Image from "./Image";
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { 
+  SignedIn, 
+  SignedOut, 
+  useAuth, 
+  UserButton 
+} from "@clerk/clerk-react";
 
 const Navbar = () => {
-  /*Create useState hook to handle the opening and closing of
-  mobile menu*/
+  // Create useState hook to handle the opening and closing of mobile menu
   const [open, setOpen] = useState(false);
+
+  // Get authentication token from react app using useAuth
+  const{ getToken } = useAuth();
+  
   return (
     /* Format the Navbar container */
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
