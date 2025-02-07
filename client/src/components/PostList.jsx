c0b26248e80667339213212df4f30c5f2492c004
mtandao centre <mtandaocentre@gmail.com>
2025-02-07 23:30:06 +0300

    Fetched data using axios and usequerry from tanstack

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,6 +1,27 @@
 import PostListItem from "./PostListItem"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 const PostList = () => {
+
+  // Use axios to fetch post
+  const fetchPosts = async () => {
+    const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`);
+    return res.data;
+  };
+
+  // Fetch data
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetchPosts(),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  console.log(data)
+
   return (
     /* - Style post list container 
        - Add more post list items to post list
