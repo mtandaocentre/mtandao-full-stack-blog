98c64365b42184753aab5dd7b38ffd003df928e1
mtandao centre <mtandaocentre@gmail.com>
2025-02-23 22:45:33 +0300

    Added infinite scroll component

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,11 +1,12 @@
 import PostListItem from "./PostListItem"
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 // Use axios to fetch post
 const fetchPosts = async (pageParam) => {
   const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
-    params : { page: pageParam },
+    params : { page: pageParam, limit: 2 },
   });
   return res.data;
 };
@@ -35,20 +36,36 @@ const PostList = () => {
 
   if (error) return 'An error has occurred: ' + error.message
 
+  // convert allPosts into a single array using flatMap
+  const allPosts = data?.pages?.flatMap((page) => page.posts) || [];
+
+  
+
   console.log(data)
 
   return (
     /* - Style post list container 
        - Add more post list items to post list
     */
-    <div className='flex flex-col gap-12 mb-8'>
+
+      <InfiniteScroll
+       dataLength={allPosts.length} 
+       next={fetchNextPage}
+       hasMore={!!hasNextPage}
+       loader={<h4>Loading more posts...</h4>}
+       endMessage={
+         <p>
+           <b>All posts loaded!</b>
+         </p>
+       }
+      >
         {/* Add PostListItem component to PostList */}
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
-    </div>
+        {/* Use  allPosts array to map postListItems */}
+        {allPosts.map((post) => (
+          <PostListItem key={post._id} post={post} />
+        ))}
+      </InfiniteScroll>
+
   )
 }
 
