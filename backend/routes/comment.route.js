9d7efdeb27ef491867450ae4e69b2d4bb772a430
mtandao centre <mtandaocentre@gmail.com>
2025-03-02 21:32:56 +0300

    Added get, post and delete methods and end points

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { addComments, deleteComments, getPostComments } from "../controllers/comment.controller.js";
 
 // Define router
 const router = express.Router();
 
 // Use router to make api call
-router.get("/comment-test", (req,res) => {
-    res.status(200).send("Comment Route")
-})
+router.get("/:postId", getPostComments)
+router.post("/:postId", addComments)
+router.delete("/:postId", deleteComments)
 
 // Export router
 export default router
