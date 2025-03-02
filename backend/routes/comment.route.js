566ab7a13aad42dbe73ad51ca3fe0fe7e12e09ce
mtandao centre <mtandaocentre@gmail.com>
2025-03-03 02:40:21 +0300

    removed plural s from  addComments and deleteComments

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { addComments, deleteComments, getPostComments } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, getPostComments } from "../controllers/comment.controller.js";
+
 
 // Define router
 const router = express.Router();
 
 // Use router to make api call
 router.get("/:postId", getPostComments)
-router.post("/:postId", addComments)
-router.delete("/:postId", deleteComments)
+router.post("/:postId", addComment)
+router.delete("/:id", deleteComment)
 
 // Export router
 export default router
