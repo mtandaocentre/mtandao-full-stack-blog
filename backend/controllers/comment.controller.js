a7750dc4ea412ab60b42a27be59d19a767733a8a
mtandao centre <mtandaocentre@gmail.com>
2025-03-06 19:38:13 +0300

    created addComment method

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import Comment from "../models/comment.model.js"
+import User from "../models/user.model.js"
 
 export const getPostComments = async (req,res) => {
     const comments = await Comment.find({post:req.params.postId})
@@ -9,7 +10,24 @@ export const getPostComments = async (req,res) => {
 }
 
 export const addComment = async (req,res) => {
-    
+    const clerkUserId = req.auth.userId;
+    const postId = req.params.postId;
+
+    if(!clerkUserId){
+        return res.status(401).json("Not Authenticated!");
+    };
+
+    const user = User.findOne({clerkUserId});
+
+    const newComment = new Comment({
+        ...req.body,
+        user: user._id,
+        post: postId,
+    });
+
+    const savedComment = await newComment.save();
+
+    res.status(201).json(savedComment);
 }
 
 export const deleteComment = async (req,res) => {
