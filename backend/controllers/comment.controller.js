dedb4e334f050991b03062c7977a649492433017
mtandao centre <mtandaocentre@gmail.com>
2025-03-06 19:51:04 +0300

    Created deleteComment method

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -2,14 +2,17 @@ import Comment from "../models/comment.model.js"
 import User from "../models/user.model.js"
 
 export const getPostComments = async (req,res) => {
+    
     const comments = await Comment.find({post:req.params.postId})
         .populate("user","username img")
-        .sort({createdAt: -1})
+        .sort({createdAt: -1});
 
-    res.json(comments)
-}
+    res.json(comments);
+
+};
 
 export const addComment = async (req,res) => {
+    
     const clerkUserId = req.auth.userId;
     const postId = req.params.postId;
 
@@ -31,5 +34,25 @@ export const addComment = async (req,res) => {
 }
 
 export const deleteComment = async (req,res) => {
+
+    const clerkUserId = req.auth.userId;
+    const id = req.params.id;
+
+    if(!clerkUserId){
+        return res.status(401).json("Not Authenticated!");
+    };
+
+    const user = User.findOne({clerkUserId});
+
+    const deletedComment = await Comment.findOneAndDelete({
+        _id: id,
+        user: user._id,
+    });
+
+    if(!deletedComment){
+        res.status(403).json("You can only delete your comment");
+    };
+
+    res.status(200).json("Your comment was susccessfully deleted!");
     
 }
